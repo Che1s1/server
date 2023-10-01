d59f7ed297c95d8b74e89618466822a8c50c882b
Che1s1 <146632339+Che1s1@users.noreply.github.com>
2023-10-01 13:38:24 +0300

    Add files via upload

---
+++ db.config.js
@@ -0,0 +1,29 @@
+module.exports = {
+    HOST: "dpg-ckckjvmct0pc738b5hvg-a",
+    USER: "grady",
+    PORT: 5432,
+    PASSWORD: "YKGobXWreSV1riQrdqRtiDW5LzymObyK",
+    DB: "che1s1db",
+    dialect: "postgres",
+    pool: {
+        max: 5,
+        min: 0,
+        acquire: 30000,
+        idle: 10000
+    }
+};
+//
+// module.exports = {
+//     HOST: "localhost",
+//     USER: "postgres",
+//     PORT: 5432,
+//     PASSWORD: "postgres",
+//     DB: "boomboom",
+//     dialect: "postgres",
+//     pool: {
+//         max: 5,
+//         min: 0,
+//         acquire: 30000,
+//         idle: 10000
+//     }
+// };
